1b2562b543689a3018222debad79a2e848addccc
tysophearum <tysophearum91@gmail.com>
2024-02-18 20:38:17 +0700

    add filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, Button } from '@nextui-org/react';
 import ItemCard from './components/ItemCard';
 import Header from './components/Header';
+import { TrendingIcon } from './icons/TrendingIcon';
+import { StarIcon } from './icons/StarIcon';
 
 function App() {
 
@@ -9,6 +11,17 @@ function App() {
     <NextUIProvider>
       <Header />
       <div className='p-3 grid grid-cols-1 place-items-center w-[100vw]'>
+        <div className='my-5 flex justify-start xl:w-[1100px] lg:w-[950px] md:w-[720px] sm:w-[80%] w-[80vw]'>
+          <Button className='mx-1' radius='full' color="primary" variant="bordered" startContent={<TrendingIcon />}>
+            Trending
+          </Button> 
+          <Button className='mx-1' radius='full' color="primary" variant="faded">
+            New
+          </Button> 
+          <Button className='mx-1' radius='full' color="primary" variant="faded" startContent={<StarIcon />}>
+            Most liked
+          </Button> 
+        </div>
         <ItemCard />
         <div className='h-8'></div>
         <ItemCard />
